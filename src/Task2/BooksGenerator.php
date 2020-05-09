5e09dc688cd378d4e52d034f6fb4e191f7f41a01
Andrii-Dovhopoliuk <57150972+Andrii-Dovhopoliuk@users.noreply.github.com>
2020-05-09 12:19:17 +0300

    Completed class Book

--- src/Task2/BooksGenerator.php
+++ src/Task2/BooksGenerator.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\Task2;
 
-class BooksGenerator
-{
-    public function generate(): \Generator
-    {
+class BooksGenerator{
+    protected int $minPagesNumber;
+    protected int $maxPrice;
+    protected array $libraryBooks = [];
+    protected array $storeBooks = [];
+
+    public function __construct(int $minPagesNumber,int $maxPrice,array $libraryBooks,array $storeBooks){
+        $this->minPagesNumber = $minPagesNumber;
+        $this->maxPrice = $maxPrice;
+        $this->libraryBooks = $libraryBooks;
+        $this->storeBooks = $storeBooks;
+    }
+
+    public function generate(): \Generator{
         //@todo
     }
 }
