5f03aab0744ad8b0059d3deb178cbe84804e9365
Andrii-Dovhopoliuk <57150972+Andrii-Dovhopoliuk@users.noreply.github.com>
2020-05-09 16:47:23 +0300

    Completed class CarTrackHtmlPresenter

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -7,10 +7,21 @@ namespace App\Task3;
 use App\Task1\Car;
 use App\Task1\Track;
 
-class CarTrackHtmlPresenter
-{
-    public function present(Track $track): string
-    {
-        //@todo
+class CarTrackHtmlPresenter{
+    public function present(Track $track): string{
+        $content = '<div class="center">';
+        $cars = $track->all();
+        foreach ($cars as $k){
+            $content .= '<div class="car">';
+            $content .= '<p class="name">'. $k->getName() .'</p>';
+            $content .= '<img class="img1" src="'.$k->getImage().'" alt="">';
+            $content .= ' <div class="info">Speed:' . $k->getSpeed() . '</div>';
+            $content .= ' <div class="info">pitStopTime:' . $k->getPitStopTime() . '</div>';
+            $content .= ' <div class="info">fuelConsumption:' . $k->getFuelConsumption() . '</div>';
+            $content .= '</div>';
+        }
+        $content .= '</div>';
+        return $content;
     }
+
 }
