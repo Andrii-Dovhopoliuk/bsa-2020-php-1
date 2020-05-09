0fe6db00afc7e49cf0ed85f3716b188b9500398f
Andrii-Dovhopoliuk <57150972+Andrii-Dovhopoliuk@users.noreply.github.com>
2020-05-09 17:02:25 +0300

    Updare css

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -13,11 +13,12 @@ class CarTrackHtmlPresenter{
         $cars = $track->all();
         foreach ($cars as $k){
             $content .= '<div class="car">';
-            $content .= '<p class="name">'. $k->getName() .'</p>';
+            $content .= '<p class="name">' . $k->getName() .'</p>';
             $content .= '<img class="img1" src="'.$k->getImage().'" alt="">';
-            $content .= ' <div class="info">Speed:' . $k->getSpeed() . '</div>';
-            $content .= ' <div class="info">pitStopTime:' . $k->getPitStopTime() . '</div>';
-            $content .= ' <div class="info">fuelConsumption:' . $k->getFuelConsumption() . '</div>';
+            $content .= ' <div class="info">Speed: ' . $k->getSpeed() . ' km/h' .'</div>';
+            $content .= ' <div class="info">Pit Stop Time: ' . $k->getPitStopTime() . ' seconds' .'</div>';
+            $content .= ' <div class="info">Fuel Consumption: ' . $k->getFuelConsumption() . ' liters'. '</div>';
+            $content .= ' <div class="info">Fuel Tank Volume: ' . $k->getFuelTankVolume() . ' liters'. '</div>';
             $content .= '</div>';
         }
         $content .= '</div>';
