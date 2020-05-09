25d6ab57c45ccdf1efcd010a7af842ea6b25952d
Andrii-Dovhopoliuk <57150972+Andrii-Dovhopoliuk@users.noreply.github.com>
2020-05-09 22:07:51 +0300

    Update class CarTrackHtmlPresenter

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -14,15 +14,14 @@ class CarTrackHtmlPresenter{
         foreach ($cars as $k){
             $content .= '<div class="car">';
             $content .= '<p class="name">' . $k->getName() .'</p>';
-            $content .= '<img class="img1" src="'.$k->getImage().'" alt="">';
-            $content .= ' <div class="info">Speed: ' . $k->getSpeed() . ' km/h' .'</div>';
-            $content .= ' <div class="info">Pit Stop Time: ' . $k->getPitStopTime() . ' seconds' .'</div>';
-            $content .= ' <div class="info">Fuel Consumption: ' . $k->getFuelConsumption() . ' liters'. '</div>';
-            $content .= ' <div class="info">Fuel Tank Volume: ' . $k->getFuelTankVolume() . ' liters'. '</div>';
+            $content .= '<img src="'.$k->getImage().'">';
+            $content .= '<div class="info">Speed: ' . $k->getSpeed() . ' km/h' .'</div>';
+            $content .= '<div class="info">Pit Stop Time: ' . $k->getPitStopTime() . ' seconds' .'</div>';
+            $content .= '<div class="info">Fuel Consumption: ' . $k->getFuelConsumption() . ' liters'. '</div>';
+            $content .= '<div class="info">Fuel Tank Volume: ' . $k->getFuelTankVolume() . ' liters'. '</div>';
             $content .= '</div>';
         }
         $content .= '</div>';
         return $content;
     }
-
 }
